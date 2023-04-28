c99c1a7393f10a9ae8a6ff1a8556adf8cd6cf434
Dreizer <artem39r@mail.ru>
2023-04-28 02:37:03 +0200

    mobile menu - done (+govnokod)

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,18 +1,13 @@
 import * as utils from "./utils.js";
 import { useDynamicAdapt } from "./dynamicAdapt.js";
-// import { burgerToggler } from "./menuBurger.js";
 
 const header = document.querySelector(`.header`);
 const phoneSpoiler = document.querySelector(".header-actions__spoiler-arrow");
 const phoneOrder = document.querySelector(".header-actions__phone");
-const catalogLinks = document.querySelectorAll(".menu-catalog__link");
 const catalog = document.querySelector(`.menu-catalog__list`);
 
-// window.catalog = catalog;
-
 const menu = document.querySelector(`.header-top-menu`);
-
-// burgerToggler(menuBurger, menu);
+const subMenu = document.querySelector(`.sub-menu-catalog`);
 
 useDynamicAdapt("max");
 
@@ -23,10 +18,19 @@ function headerActions(e) {
 
   if (targetElement.closest(".menu-burger")) {
     if (targetElement.closest(".menu-burger--active")) {
+      const activeLink = catalog.querySelector(`.menu-catalog__link--active`);
+      const activeBlock = subMenu.querySelector(`.sub-menu-catalog__block--active`);
+
       targetElement.closest(".menu-burger").classList.remove(`menu-burger--active`);
       document.body.classList.remove(`scroll-lock`);
       menu.classList.remove(`header-top-menu--active`);
       catalog.classList.remove(`header-top-menu__sub-menu--active`);
+      if (activeLink) {
+        activeLink.classList.remove(`menu-catalog__link--active`);
+      }
+      if (activeBlock) {
+        activeBlock.classList.remove(`sub-menu-catalog__block--active`);
+      }
     } else {
       targetElement.closest(".menu-burger").classList.add(`menu-burger--active`);
       document.body.classList.add(`scroll-lock`);
@@ -35,9 +39,12 @@ function headerActions(e) {
   }
 
   if (targetElement.closest(".back")) {
-    utils
-      .findParent(targetElement, `header-top-menu__sub-menu--active`)
-      .classList.remove(`header-top-menu__sub-menu--active`);
+    const parentofTarget = utils.findParent(targetElement, `header-top-menu__sub-menu--active`)
+      ? utils.findParent(targetElement, `header-top-menu__sub-menu--active`)
+      : utils.findParent(targetElement, `sub-menu-catalog__block--active`);
+    const activeBlock = document.querySelector(`.sub-menu-catalog__block--active`);
+    parentofTarget.classList.remove(`header-top-menu__sub-menu--active`, `sub-menu-catalog__block--active`);
+    if (activeBlock) activeBlock.classList.remove("sub-menu-catalog__block--active");
   }
 
   if (targetElement.closest("[data-catalog-parent]")) {
@@ -49,7 +56,7 @@ function headerActions(e) {
       const activeLink = document.querySelector(`.menu-catalog__link--active`);
       const activeBlock = document.querySelector(`.sub-menu-catalog__block--active`);
 
-      if (activeLink && targetElement !== activeLink) {
+      if (activeLink && activeBlock && targetElement !== activeLink) {
         activeLink.classList.remove(`menu-catalog__link--active`);
         activeBlock.classList.remove("sub-menu-catalog__block--active");
       }

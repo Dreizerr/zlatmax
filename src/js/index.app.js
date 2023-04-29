6f7041a291e23590620d2ae391f3a155a779f5d9
Dreizer <artem39r@mail.ru>
2023-04-29 14:28:50 +0200

    intro

--- src/js/index.app.js
+++ src/js/index.app.js
@@ -1,4 +1,16 @@
 import * as utils from "./modules/utils.js";
 import * as headerJs from "./modules/header.js";
 
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js";
+
+const introSwiper = new Swiper(".intro__swiper", {
+  // autoHeight: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  speed: 1000,
+  loop: true,
+});
+
 utils.isWebp();

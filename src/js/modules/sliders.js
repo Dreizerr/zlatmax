a58816b7e2e470de3ead031161da5c4c9452cea1
Dreizer <artem39r@mail.ru>
2023-05-10 23:37:54 +0200

    catalog cards

---
+++ src/js/modules/sliders.js
@@ -0,0 +1,44 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js";
+
+if (document.querySelector(`.intro__swiper`)) {
+  const introSwiper = new Swiper(".intro__swiper", {
+    spaceBetween: 150,
+    simulateTouch: false,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".intro__swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    speed: 1500,
+    loop: true,
+    breakpoints: {},
+
+    on: {
+      slideChange: function () {
+        const fracCurrent = document.querySelector(`.intro__swiper-frac-current`);
+        const fracAll = document.querySelector(`.intro__swiper-frac-all`);
+        let currentSlide = this.realIndex + 1;
+
+        currentSlide = currentSlide < 10 ? `0${currentSlide}` : currentSlide;
+        fracCurrent.textContent = currentSlide;
+      },
+
+      afterInit: function () {
+        const fracCurrent = document.querySelector(`.intro__swiper-frac-current`);
+        const fracAll = document.querySelector(`.intro__swiper-frac-all`);
+        let currentSlide = this.realIndex + 1;
+        currentSlide = currentSlide < 10 ? `0${currentSlide}` : currentSlide;
+        fracCurrent.textContent = currentSlide;
+
+        const introSwiperDOM = document.querySelector(`.intro__swiper`);
+        let allSlides = introSwiperDOM.querySelectorAll(`.swiper-slide`).length;
+        allSlides = allSlides < 10 ? `0${allSlides}` : allSlides;
+        fracAll.textContent = allSlides;
+      },
+    },
+  });
+}
